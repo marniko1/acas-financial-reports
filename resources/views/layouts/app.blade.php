8660a4acf96621e14fbbb484da954dfc5066935c
marniko1 <zmajeviognjenogvuka@gmail.com>
2019-08-15 09:00:59 +0200

    cleaning views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,13 +41,13 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav mr-auto">
-                            {{-- @auth --}}
+                            @auth
                             <li class="nav-item"><a class="nav-link" href="#">Link1</a></li>
                             <li class="nav-item"><a class="nav-link" href="#">Link2</a></li>
                             <li class="nav-item"><a class="nav-link" href="#">Link3</a></li>
                             <li class="nav-item"><a class="nav-link" href="#">Link4</a></li>
                             <li class="nav-item"><a class="nav-link" href="#">Link5</a></li>
-                            {{-- @endauth --}}
+                            @endauth
                         </ul>
 
                         <!-- Right Side Of Navbar -->
